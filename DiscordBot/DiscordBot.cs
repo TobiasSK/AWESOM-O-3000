d275f95312d3071078cf3016f5210e7638e44b1c
Minko Todorov <kunobelin@yahoo.com>
2017-01-24 02:12:58 +0100

    first commit

---
+++ DiscordBot/DiscordBot.cs
@@ -0,0 +1,51 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+
+namespace DiscordBot
+{
+    class DiscordBot
+    {
+        DiscordClient client;
+        CommandService commands;
+
+        public DiscordBot()
+        {
+            
+
+            client = new DiscordClient(input =>
+            {
+                input.LogLevel = LogSeverity.Info;
+                input.LogHandler = Log;
+            });
+
+            
+
+            client.UsingCommands(input => {
+
+                
+                input.PrefixChar = '!';
+                input.AllowMentionPrefix = true;
+            });
+
+            commands = client.GetService<CommandService>();
+
+            commands.CreateCommand("Hello").Do(async (e) =>
+            {
+                await e.Channel.SendMessage("World");
+            });
+
+            client.ExecuteAndWait(async() =>
+                { 
+                await client.Connect("MTk3NDUzODUwNDY5MzM1MDUx.C2g1eg.SJtbmHXDhsIbxorIXjkxQV2VDLs", TokenType.Bot);
+                });
+
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+
+        }
+    }
+}
