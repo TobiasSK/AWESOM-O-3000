c7ce93a48a65e6ff001ed2c186b381af878771f2
Minko Todorov <kunobelin@yahoo.com>
2017-01-30 00:59:51 +0100

    announce working with user messages

--- DiscordBot/DiscordBot.cs
+++ DiscordBot/DiscordBot.cs
@@ -2,6 +2,7 @@
 using Discord.Commands;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace DiscordBot
 {
@@ -46,13 +47,18 @@ namespace DiscordBot
                 await e.Channel.SendMessage("Nigga, please!");
             });
 
+            commands.CreateCommand("announce").Parameter("message", ParameterType.Unparsed).Do(async (e) =>
+            {
+                await DoAnnouncement(e);
+            });
+
             client.UserJoined += async (s, e) =>
             {
                 var channel = e.Server.FindChannels("gay-mers", ChannelType.Text).FirstOrDefault();
 
                 var user = e.User;
 
-                await channel.SendMessage(string.Format("{0} has joined the channel!", user.Name));
+                await channel.SendTTSMessage(string.Format("{0} has joined the channel!", user.Name));
             };
 
             client.UserLeft += async (s, e) =>
@@ -61,7 +67,7 @@ namespace DiscordBot
 
                 var user = e.User;
 
-                await channel.SendMessage(string.Format("{0} has left the channel", user.Name));
+                await channel.SendTTSMessage(string.Format("{0} has left the channel", user.Name));
             };
 
 
@@ -70,6 +76,42 @@ namespace DiscordBot
                 await client.Connect("MTk3NDUzODUwNDY5MzM1MDUx.C2g1eg.SJtbmHXDhsIbxorIXjkxQV2VDLs", TokenType.Bot);
                 });
 
+        }
+
+        private async Task DoAnnouncement(CommandEventArgs e)
+        {
+            var channel = e.Server.FindChannels(e.Args[0], ChannelType.Text).FirstOrDefault();
+
+            var message = ConstructMessage(e, channel != null);
+
+            if (channel != null)
+            {
+                await channel.SendMessage(message);
+            }
+            else
+            {
+                await e.Channel.SendMessage(message);
+            }
+        }
+
+        private string ConstructMessage(CommandEventArgs e, bool firstArgIsChannel)
+        {
+            string message = "";
+
+            var name = e.User.Nickname != null ? e.User.Nickname : e.User.Name;
+
+            var starIndex = firstArgIsChannel ? 1 : 0;
+
+            for (int i = starIndex; i < e.Args.Length; i++)
+            {
+                message += e.Args[i].ToString() + " ";
+            }
+
+            var result = name + " says: " + message;
+
+            return result;
+
+            
         }
 
         private void Log(object sender, LogMessageEventArgs e)
